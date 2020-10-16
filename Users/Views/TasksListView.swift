ae13da30e135069a2338df2335fb3f6429e28ae1
Mohcine Belarrem <belarrem.mohcine@gmail.com>
2020-10-16 20:50:23 +0100

    Fetches Tasks Data using selected user id

--- Users/Views/TasksListView.swift
+++ Users/Views/TasksListView.swift
@@ -9,15 +9,72 @@ import SwiftUI
 
 struct TasksListView: View {
     
-    var user : User
+    @State var user : User
+    
+    @State var tasks = [Task]()
     
     var body: some View {
-        Text("Displaying Tasks for \(user.name)")
+        
+        List() {
+            
+            ForEach(tasks) { task in
+                Section() {
+                       TaskView(task: task)
+                }
+            }
+        }
+        .listStyle(GroupedListStyle())
+        .onAppear(perform: {fetchTasks(userId: "\(user.id)")})
+        .navigationBarTitle("Tasks",displayMode: .inline)
+        
+    }
+    
+    
+     func fetchTasks(userId: String) {
+        
+        var components = URLComponents()
+        
+        components.scheme = NetworkModel.scheme
+        components.host = NetworkModel.baseHost
+        components.path = NetworkModel.tasksPath
+        components.queryItems = [NetworkModel.tasksQueryItem(userId: userId)]
+        
+        guard let url = components.url else {
+            //self.finishedLoading = true
+            //noData = true
+            return
+        }
+        
+        //print(url)
+        
+        let request = URLRequest(url: url)
+        
+        URLSession.shared.dataTask(with: request) { data, response, error in
+            
+            guard let data = data else {
+                //self.finishedLoading = true
+                //noData = true
+                return
+            }
+            
+            if let tasks = try? JSONDecoder().decode([Task].self, from: data) {
+                DispatchQueue.main.async {
+                    self.tasks = tasks
+                }
+                //self.finishedLoading = true
+                //noData = false
+                return
+            }
+            
+            //noData = true
+            print("Failure : \(error?.localizedDescription ?? "Unknown error")")
+        }.resume()
+        
     }
 }
 
 struct TasksListView_Previews: PreviewProvider {
     static var previews: some View {
-        TasksListView(user: dummyUser)
+        TasksListView(user: dummyUser, tasks: dummyTasksList)
     }
 }
