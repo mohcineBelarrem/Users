8ad1f0d8746cbc744ba0025784c55f43de657e84
Mohcine Belarrem <belarrem.mohcine@gmail.com>
2020-10-16 17:10:18 +0100

    First Commit

---
+++ Users/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  Users
+//
+//  Created by Mohcine BELARREM on 16/10/2020.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
