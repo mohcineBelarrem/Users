d7fea0a6860e1111f1d289077a922ea024aae79f
Mohcine Belarrem <belarrem.mohcine@gmail.com>
2020-10-16 17:56:28 +0100

    Fetches the list of users using the url, then displays each user's name on a list

--- Users/Views/UsersListView.swift
+++ Users/Views/UsersListView.swift
@@ -8,20 +8,70 @@
 import SwiftUI
 
 struct UsersListView: View {
+    
+    //@State var finishedLoading = true
+    
+    //@State var noData = false
+    
+    @State var users : [User]
+    
     var body: some View {
        
-        List() {
-            Text("User 1")
-            Text("User 2")
-            Text("User 3")
-            Text("User 4")
-        }
+        List(users) { user in
+        
+            Text("\(user.name)")
+        
+        }.onAppear(perform: {fetchUsers()})
     }
+    
+    
+    
+    func fetchUsers() {
+        
+        var components = URLComponents()
+        
+        components.scheme = NetworkModel.scheme
+        components.host = NetworkModel.baseHost
+        components.path = NetworkModel.usersPath
+        
+        guard let url = components.url else {
+            //self.finishedLoading = true
+            //noData = true
+            return
+        }
+        
+        print(url)
+        
+        let request = URLRequest(url: url)
+        
+        URLSession.shared.dataTask(with: request) { data, response, error in
+            
+            guard let data = data else {
+                //self.finishedLoading = true
+                //noData = true
+                return
+            }
+            
+            if let users = try? JSONDecoder().decode([User].self, from: data) {
+                DispatchQueue.main.async {
+                    self.users = users
+                }
+                //self.finishedLoading = true
+                //noData = false
+                return
+            }
+            
+            //noData = true
+            print("Failure : \(error?.localizedDescription ?? "Unknown error")")
+        }.resume()
+        
+    }
+    
     
 }
 
 struct UsersListView_Previews: PreviewProvider {
     static var previews: some View {
-        UsersListView()
+        UsersListView( users: [User]())
     }
 }
