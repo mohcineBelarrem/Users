8c750be67ef80d8e772cba5c3a3e16fd4b0432ef
Mohcine Belarrem <belarrem.mohcine@gmail.com>
2020-10-17 16:15:17 +0100

    Makes the AppModel contact the realm database

--- Users/Model/AppModel.swift
+++ Users/Model/AppModel.swift
@@ -6,6 +6,7 @@
 //
 
 import Foundation
+import RealmSwift
 
 
 
@@ -13,6 +14,43 @@ class AppModel : ObservableObject {
     
     @Published var isTaskListViewShown = false
     
-    @Published var selectedUser = dummyUser
+    @Published var selectedUser = User.dummyUser()
     
+    var realm : Realm!
+    
+    func openRealm() {
+        do {
+            
+            realm = try Realm()
+            
+        }
+        catch {
+            print("We can't open a new Realm #teslim 🐐 👿")
+        }
+    }
+    
+    func saveObject(object : Object) {
+        do {
+            try realm.write{
+                realm.add(object)
+            }
+        }
+        catch {
+            print("Ljwad didn't let us use the realm \(error.localizedDescription)")
+        }
+    }
+    
+    func getUsers() -> [User] {
+        
+        var users = [User]()
+        
+        let objects =  realm.objects(User.self)
+        
+        for object in objects {
+            
+            users.append(object)
+        }
+        
+        return users
+    }
 }
